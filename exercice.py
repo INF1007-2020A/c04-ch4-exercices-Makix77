f2ac5ef05872a90f8c23675b4f029fe4d65a6890
Makix77 <olivier2000_9@hotmail.ca>
2020-09-15 22:48:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def is_even_len(string: str) -> bool:
 def remove_third_char(string: str) -> str:
     newChar = ''
     for c in range(0, len(str))
-        print(s)
+        print(str`<¸^)
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
